7e9b7d51c2ee0ee2fa0731693323cd4c0adb556b
James Clover <jamesmclover93@gmail.com>
2024-02-29 13:09:16 -0500

    creating server skeleton

---
+++ example.config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+// Instructions
+// 1) Rename "example.config.js" to "config.js"
+// 2) Replace the gitToken with your own GitHub Token in string form.
+
+module.exports = {
+  GITHUB_APIKEY: process.env.GITHUB_APIKEY
+};
