1f93c560b8a3398b8a8d0b32b3d894027b723173
Stephen Capper <stephencapper@gmail.com>
2024-03-01 10:36:59 -0800

    Initial work on R&R components

---
+++ src/components/rating-reviews/components/RatingBreakdown.jsx
@@ -0,0 +1,12 @@
+install React, { useState } from "react";
+
+const RatingReviews = ( {product_id} ) => {
+
+  return(
+  <aside className="rating-breakdown">
+    <p>Rating breakdown placeholder</p>
+  </aside>
+  );
+};
+
+export default RatingsReviews;
