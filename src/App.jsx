a212a449a9474a6bd0be66522f181d231b43b172
Stephen Capper <stephencapper@gmail.com>
2024-03-01 15:19:13 -0800

    revert row 1 of src/App.jsx file t previous version (unintentional change)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 // Template: Uncomment and change when needed
 // import ProductsInfo from 'ProductsInfo';
 // import RelatedProducts from 'RelatedProducts';
