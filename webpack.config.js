a9ec83342ca8fefc27c84f034b3b9e519e91ef94
greg <greg.jenkins1034@gmail.com>
2024-03-01 12:25:25 -0600

    DotEnv added to package-json

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   name: 'webpack-config-Atelier',
   mode: 'development',
-  entry: path.join(__dirname, './src', './index.js'),
+  entry: path.join(__dirname, './src', './index.jsx'),
   output: {
     path: path.resolve(__dirname, './dist'),
   },
