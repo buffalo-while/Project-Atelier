8f547b96fd8e594fe36e28ceb2b36a5ad4d85102
greg <greg.jenkins1034@gmail.com>
2024-02-29 21:56:17 -0600

    Added webpack.config.js

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./assets/base.css";
 import "./assets/styles.css";
 import { render } from "react-dom";
-import App from "./components/App.jsx";
+import App from "./App.jsx";
 
 const root = document.createElement("div");
 root.setAttribute("id", "root");
