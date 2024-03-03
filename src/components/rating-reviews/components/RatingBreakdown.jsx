b67229f50348a0ff596e12c0fcccf47f5c70af42
Stephen Capper <stephencapper@gmail.com>
2024-03-02 18:49:41 -0800

    update RatingBreakdown to use getRatings

--- src/components/rating-reviews/components/RatingBreakdown.jsx
+++ src/components/rating-reviews/components/RatingBreakdown.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getReviewsMetaData } from "../models/reviewsModels.js";
 
-const RatingBreakdown = ( {product_id} ) => {
+const RatingBreakdown = ({ productId, getRatings }) => {
+  const [ratingStars, setRatingStars] = useState(<p className={"stars"}>Loading</p>);
+
+  useEffect(()=> {
+    getRatings(productId)
+    .then((metaResults) => {
+      console.log('Got results from promise: ', metaResults);
+      setRatingStars(metaResults.RatingStars);
+    })
+  }, []);
+
 
   return(
   <aside className="rating-breakdown">
-    <p>Rating breakdown placeholder</p>
+    {ratingStars}
   </aside>
   );
 };
