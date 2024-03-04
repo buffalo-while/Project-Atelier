0cf72bd16a7aa11c8d14fe4ed1e04a94826d7a92
greg <greg.jenkins1034@gmail.com>
2024-03-04 14:03:32 -0600

    Image Gallery looking much better, aligned thumbnails to main hero image.

--- src/components/overview/OverviewMain.jsx
+++ src/components/overview/OverviewMain.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ImageGallery from './ImageGallery.jsx'
+import ProdInfo from './ProdInfo.jsx'
 
 function OverviewMain({ productId }) {
   return (
     <div>
       <h1>productId: {productId}</h1>
-      <ImageGallery productId={productId}/>
+      <div className="content-container-1">
+      <ImageGallery className="image-gallery" productId={productId}/>
+      <ProdInfo className="product-info" />
+      </div>
     </div>
   );
 }
